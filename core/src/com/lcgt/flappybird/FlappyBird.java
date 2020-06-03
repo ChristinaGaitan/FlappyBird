fc9324f53753ee6a1079dbd1dd2ab89b971ae70b
ChristinaGaitan <christina.gaitan@gmail.com>
2020-06-03 12:07:52 -0500

    Add assets and display background

--- core/src/com/lcgt/flappybird/FlappyBird.java
+++ core/src/com/lcgt/flappybird/FlappyBird.java
@@ -9,30 +9,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
-	Texture bird;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
-		bird = new Texture("bird.png");
 	}
 
 	@Override
 	public void render () {
-		Integer screenwidth = Gdx.graphics.getWidth();
-		Integer height = Gdx.graphics.getHeight();
+		Integer screenWidth = Gdx.graphics.getWidth();
+		Integer screenHeight = Gdx.graphics.getHeight();
 
 		batch.begin();
-		batch.draw(background, 0, 0, width, height);
-		batch.draw(background, width / 2 , height / 2);
+		batch.draw(background, 0, 0, screenWidth, screenHeight);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		background.dispose();
-		bird.dispose();
 	}
 }
