db918610feab748c2734d8acb392195dd468ee65
ChristinaGaitan <christina.gaitan@gmail.com>
2020-06-03 12:12:13 -0500

    Show birds in the center of the screen

--- core/src/com/lcgt/flappybird/FlappyBird.java
+++ core/src/com/lcgt/flappybird/FlappyBird.java
@@ -7,28 +7,46 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture background;
+    SpriteBatch batch;
+    Texture background;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		background = new Texture("bg.png");
-	}
+    Texture[] birds;
+    int flpstate = 0;
 
-	@Override
-	public void render () {
-		Integer screenWidth = Gdx.graphics.getWidth();
-		Integer screenHeight = Gdx.graphics.getHeight();
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        background = new Texture("bg.png");
+        birds = new Texture[2];
+        birds[0] = new Texture("bird.png");
+        birds[1] = new Texture("bird2.png");
+    }
 
-		batch.begin();
-		batch.draw(background, 0, 0, screenWidth, screenHeight);
-		batch.end();
-	}
+    @Override
+    public void render () {
 
-	@Override
-	public void dispose () {
-		batch.dispose();
-		background.dispose();
-	}
+        if (flpstate == 0) {
+            flpstate = 1;
+        } else {
+            flpstate = 0;
+        }
+
+        Integer screenWidth = Gdx.graphics.getWidth();
+        Integer screenHeight = Gdx.graphics.getHeight();
+        Integer birdWidth = birds[flpstate].getWidth();
+        Integer birdHeight = birds[flpstate].getHeight();
+
+        batch.begin();
+        batch.draw(background, 0, 0, screenWidth, screenHeight);
+        batch.draw(birds[flpstate], screenWidth / 2 - birdWidth / 2, screenHeight / 2 - birdHeight / 2);
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        background.dispose();
+        birds[0].dispose();
+        birds[1].dispose();
+    }
 }
