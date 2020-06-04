85003c279d0dddc800b8ba37c6cf172ab75cb0cd
ChristinaGaitan <christina.gaitan@gmail.com>
2020-06-04 11:07:10 -0500

    Move calculations to create method and react to tap

--- core/src/com/lcgt/flappybird/FlappyBird.java
+++ core/src/com/lcgt/flappybird/FlappyBird.java
@@ -9,9 +9,16 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class FlappyBird extends ApplicationAdapter {
     SpriteBatch batch;
     Texture background;
+    float screenWidth = 0;
+    float screenHeight = 0;
 
     Texture[] birds;
     int flpstate = 0;
+    float birdY = 0;
+    float birdX = 0;
+    float velocity = 0;
+
+    int gameState = 0;
 
     @Override
     public void create () {
@@ -20,10 +27,32 @@ public class FlappyBird extends ApplicationAdapter {
         birds = new Texture[2];
         birds[0] = new Texture("bird.png");
         birds[1] = new Texture("bird2.png");
+
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
+
+        float birdWidth = birds[0].getWidth();
+        float birdHeight = birds[0].getHeight();
+        birdX = screenWidth / 2 - birdWidth / 2;
+        birdY = screenHeight / 2 - birdHeight / 2;
     }
 
     @Override
     public void render () {
+        if (Gdx.input.justTouched()) {
+            gameState = 1;
+            Gdx.app.log("Touched", "Yes");
+        }
+
+        if (gameState != 0) {
+            velocity++;
+            birdY -= velocity;
+        } else {
+            if (Gdx.input.justTouched()) {
+                gameState = 1;
+                Gdx.app.log("Touched", "Yes");
+            }
+        }
 
         if (flpstate == 0) {
             flpstate = 1;
@@ -31,14 +60,9 @@ public class FlappyBird extends ApplicationAdapter {
             flpstate = 0;
         }
 
-        Integer screenWidth = Gdx.graphics.getWidth();
-        Integer screenHeight = Gdx.graphics.getHeight();
-        Integer birdWidth = birds[flpstate].getWidth();
-        Integer birdHeight = birds[flpstate].getHeight();
-
         batch.begin();
         batch.draw(background, 0, 0, screenWidth, screenHeight);
-        batch.draw(birds[flpstate], screenWidth / 2 - birdWidth / 2, screenHeight / 2 - birdHeight / 2);
+        batch.draw(birds[flpstate], birdX, birdY);
         batch.end();
     }
 
